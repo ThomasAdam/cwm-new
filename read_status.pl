80ae75739772250bf7070bd41f9ca516f1a6af39
Thomas Adam <thomas@xteddy.org>
2013-12-25 18:00:46 +0000

    Reindent read_status.pl
    This now uses a ts/sw of 8, rather than 4.

--- read_status.pl
+++ read_status.pl
@@ -28,91 +28,96 @@ my @pipes = glob("/tmp/cwm-*.sock");
 exit unless @pipes;
 
 my %dzen_options = (
-    '-fg' => 'white',
-    '-bg' => 'blue',
-    '-ta' => 'l',
-    '-w'  => '580',
-    '-p'  => '',
+        '-fg' => 'white',
+        '-bg' => 'blue',
+        '-ta' => 'l',
+        '-w'  => '580',
+        '-p'  => '',
 );
 
 sub send_to_dzen
 {
-    my ($data, $fh) = @_;
-    my $msg;
+        my ($data, $fh) = @_;
+        my $msg;
 
-    # For the list of desktops, we maintain the sort order based on the
-    # group names being 0 -> 9.
-    foreach my $deskname (sort { $data->{'desktops'}->{$a}->{'sym_name'} <=>
-                                 $data->{'desktops'}->{$b}->{'sym_name'} }
-                                keys %{$data->{'desktops'}})
-    {
-        my $sym_name = $data->{'desktops'}->{$deskname}->{'sym_name'};
-        if (grep /^$deskname$/, @{$data->{'active_desktops'}}) {
-            $msg .= "|^bg(#007fff) $sym_name ^bg()";
-        } elsif (grep /^$deskname$/, @{$data->{'urgency'}}) {
-            $msg .= "|^bg(#7c8814) $sym_name ^bg()";
-        } elsif ($data->{'desktops'}->{$deskname}->{'count'} > 0) {
-            # Here's a way of highlighting groups with clients on them.
-            $msg .= "|^bg(#004c98) $sym_name ^bg()";
-        } elsif ($data->{'desktops'}->{$deskname}->{'count'} == 0) {
-            # Don't show groups which have no clients.
-            next;
-        } else {
-            $msg .= "|^fg() $sym_name ";
-        } 
-    }
+        # For the list of desktops, we maintain the sort order based on the
+        # group names being 0 -> 9.
+        foreach my $deskname (
+                sort {
+                        $data->{'desktops'}->{$a}->{'sym_name'} <=>
+                        $data->{'desktops'}->{$b}->{'sym_name'}
+                } keys %{$data->{'desktops'}})
+        {
+                my $sym_name = $data->{'desktops'}->{$deskname}->{'sym_name'};
+                if (grep /^$deskname$/, @{$data->{'active_desktops'}}) {
+                        $msg .= "|^bg(#007fff) $sym_name ^bg()";
+                } elsif (grep /^$deskname$/, @{$data->{'urgency'}}) {
+                        $msg .= "|^bg(#7c8814) $sym_name ^bg()";
+                } elsif ($data->{'desktops'}->{$deskname}->{'count'} > 0) {
+                        # Highlight groups with clients on them.
+                        $msg .= "|^bg(#004c98) $sym_name ^bg()";
+                } elsif ($data->{'desktops'}->{$deskname}->{'count'} == 0) {
+                        # Don't show groups which have no clients.
+                        next;
+                } else {
+                        $msg .= "|^fg() $sym_name ";
+                } 
+        }
 
-    if (defined $data->{'client'}) {
-            $msg .= "|^bg(#7f00ff)^fg()".$data->{'client'};
-    }
+        if (defined $data->{'client'}) {
+                $msg .= "|^bg(#7f00ff)^fg()".$data->{'client'};
+        }
 
-    print $fh $msg, "\n";
-    $fh->flush();
+        print $fh $msg, "\n";
+        $fh->flush();
 }
 
 sub process_line
 {
-    my ($fifo, $fh) = @_;
-    my %data = ();
+        my ($fifo, $fh) = @_;
+        my %data = ();
 
-    open (my $pipe_fh, '<', $fifo) or die "Cannot open $fifo: $!";
-    while (1) {
-        while (my $line = <$pipe_fh>) {
-            # Each element is pipe-separated.  Key/values are then
-            # comma-separated, and multiple values for those are
-            # comma-separated.
-            %data = map {
-                my ($k, $v) = split /:/, $_, 2;
-                defined $v ? ($k => $v) : ($k => '');
-            } split(/\|/, $line);
+        open (my $pipe_fh, '<', $fifo) or die "Cannot open $fifo: $!";
+        while (1) {
+                while (my $line = <$pipe_fh>) {
+                        # Each element is pipe-separated.  Key/values are then
+                        # comma-separated, and multiple values for those are
+                        # comma-separated.
+                        %data = map {
+                                my ($k, $v) = split /:/, $_, 2;
+                                defined $v ? ($k => $v) : ($k => '');
+                        } split(/\|/, $line);
 
-            # For those entries we know might contain more than one element,
-            # convert the comma-separated list in to an array.
-            exists $data{$_} and $data{$_} = [split /,/, $data{$_}]
-                for (qw/urgency desktops active_desktops/);
+                        # For those entries we know might contain more than one
+                        # element, convert the comma-separated list in to an
+                        # array.
+                        exists $data{$_} and $data{$_} = [split /,/, $data{$_}]
+                                for (qw/urgency desktops active_desktops/);
 
-            # For the list of desktops, there's both the name and the number
-            # of clients on that desk.  This can be used to flag inactive
-            # groups with clients on them.
-            $data{'desktops'} = { map {
-                ($1 => {sym_name => $2, count => $3})
-                    if /(.*?)\s+\((.*?)\)\s+\((.*?)\)/;
-            } @{$data{'desktops'}} };
+                        # For the list of desktops, there's both the name and
+                        # the number of clients on that desk.  This can be used
+                        # to flag inactive groups with clients on them.
+                        $data{'desktops'} = {
+                                map {
+                                        ($1 => {sym_name => $2, count => $3})
+                                        if /(.*?)\s+\((.*?)\)\s+\((.*?)\)/;
+                                } @{$data{'desktops'}}
+                        };
 
-            send_to_dzen(\%data, $fh);
+                        send_to_dzen(\%data, $fh);
+                }
         }
-    }
-    close ($pipe_fh);
+        close ($pipe_fh);
 }
 
 foreach (@pipes) {
-    if (fork()) {
-        # XXX: Close certain filehandles here; STDERR, etc.
-        my $dzen_cmd = "dzen2 " . join(" ", map { $_ . " $dzen_options{$_}" }
-            keys(%dzen_options));
-        my $dzen_pipe = IO::Pipe->new();
-        $dzen_pipe->writer($dzen_cmd);
- 
-        process_line($_, $dzen_pipe);
-    }
+        if (fork()) {
+                # XXX: Close certain filehandles here; STDERR, etc.
+                my $dzen_cmd = "dzen2 " . join(" ",
+                        map { $_ . " $dzen_options{$_}" } keys(%dzen_options));
+                my $dzen_pipe = IO::Pipe->new();
+                $dzen_pipe->writer($dzen_cmd);
+
+                process_line($_, $dzen_pipe);
+        }
 }
