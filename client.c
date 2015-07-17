5d231a17b5175dae559f3d48c5cac025af2d31be
Thomas Adam <thomas@xteddy.org>
2015-07-17 13:36:40 +0100

    client_expand: Fix coordinates
    When RandR is in use, ensure we reference the correct screen.

--- client.c
+++ client.c
@@ -352,8 +352,8 @@ client_expand_horiz(struct client_ctx *cc, struct geom *new_geom)
 
 	sc = cc->sc;
 
-	new_x1 = 0;
-	new_x2 = sc->work.w - (cc->bwidth * 2);
+	new_x1 = sc->view.x;
+	new_x2 = new_x1 + sc->view.w - (cc->bwidth * 2);
 
 	cc_x = new_geom->x;
 	cc_y = new_geom->y;
@@ -412,8 +412,8 @@ client_expand_vert(struct client_ctx *cc, struct geom *new_geom)
 	cc_end_x = cc_x + new_geom->w;
 	cc_end_y = cc_y + new_geom->h;
 
-	new_y1 = 0;
-	new_y2 = sc->work.h - (cc->bwidth * 2);
+	new_y1 = sc->view.y;
+	new_y2 = sc->view.h - (cc->bwidth * 2);
 
 	/* Go through all clients and move up and down. */
 	TAILQ_FOREACH(ci, &sc->clientq, entry) {
@@ -447,6 +447,8 @@ void
 client_expand(struct client_ctx *cc)
 {
 	struct geom	 new_geom;
+
+	log_debug("%s: Expanding client on screen '%s'", __func__, cc->sc->name);
 
 	if (cc->flags & CLIENT_FREEZE)
 		return;
