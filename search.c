92d3346750d08e6986d2ce7c9852cacf8be4dfb8
Thomas Adam <thomas@xteddy.org>
2015-07-22 00:07:46 +0100

    search menu: prefix entries with output name
    Mark each window as to the output it is on.

--- search.c
+++ search.c
@@ -142,8 +142,8 @@ search_print_client(struct menu *mi, int list)
 	if (list)
 		cc->matchname = cc->name;
 
-	(void)snprintf(mi->print, sizeof(mi->print), "(%d) %c[%s] %s",
-	    cc->group ? cc->group->num : 0, flag,
+	(void)snprintf(mi->print, sizeof(mi->print), "%s: (%d) %c[%s] %s",
+	    cc->sc->name, cc->group ? cc->group->num : 0, flag,
 	    cc->label ? cc->label : "", cc->matchname);
 
 	if (!list && cc->matchname != cc->name &&
