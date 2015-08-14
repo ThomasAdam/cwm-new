3086e1e0a9f53801dc9c9af6ea4ce673b6e2577f
Thomas Adam <thomas@xteddy.org>
2015-08-14 16:24:16 +0100

    put_status: change to use JSON output
    Rather than hand-crafting arbitrary-formatted strings, instead use JSON to
    define those fields.

--- util.c
+++ util.c
@@ -30,10 +30,9 @@
 #include <unistd.h>
 
 #include "calmwm.h"
+#include "parson.h"
 
 #define MAXARGLEN 20
-
-static void	 u_append_str(char **, const char *, ...);
 
 void
 u_spawn(char *argstr)
@@ -109,90 +108,56 @@ u_init_pipes(struct screen_ctx *sc)
 void
 u_put_status(struct screen_ctx *sc)
 {
-	struct client_ctx       *cc = client_current(), *ci;
-	struct group_ctx        *gc;
-	int                      client_count = 0;
-	char                    *active_groups, *all_groups, *urgencies;
+	struct client_ctx	*cc = client_current(), *ci;
+	struct group_ctx	*gc;
+	JSON_Value		*json_root = json_value_init_object();
+	JSON_Object		*json_obj = json_object(json_root);
+	JSON_Array		*clients = NULL;
+	char			*json_out_str;
+	char			 key[1024];
 
-	urgencies = all_groups = active_groups = NULL;
+	json_object_set_number(json_obj, "version", 1);
+	json_object_set_string(json_obj, "screen", sc->name);
+	json_object_set_string(json_obj, "current_client",
+				cc != NULL ? cc->name : NULL);
+	json_object_set_value(json_obj, "groups", json_value_init_object());
 
-	if (sc->status_fp == NULL)
-	        return;
-
-	fprintf(sc->status_fp, "screen:%s|", sc->name);
-
-	/*
-	 * Go through all clients regardless of the group.  Looking at all
-	 * clients will allow for catching urgent clients on other groups which
-	 * might not be active.
-	 */
-	TAILQ_FOREACH(ci, &sc->clientq, entry) {
-	        if (cc != NULL && ci == cc)
-	                fprintf(sc->status_fp, "client:%s|", cc->name);
-	        if (ci->flags & CLIENT_URGENCY) {
-	                u_append_str(&urgencies, "%s,",
-	                        (ci->group != NULL && ci->group->name) ?
-				ci->group->name : "nogroup");
-	        }
-	}
-
-	/* Now go through all groups and find how many clients are on each.
-	 * This is useful so that status indicators can mark groups as having
-	 * clients on them, if they're not the active group(s); making a
-	 * distinction between empty and occupied groups, for example.
+	/* Go through all groups, and all clients on those groups, and report
+	 * back information about the state of both.
 	 */
 	TAILQ_FOREACH(gc, &sc->groupq, entry) {
-	        client_count = 0;
-	        TAILQ_FOREACH(ci, &gc->clientq, group_entry)
-	                client_count++;
-	        if (gc->flags & GROUP_ACTIVE) {
-	                u_append_str(&active_groups, "%s,",
-	                        gc->name);
-	        }
-	        u_append_str(&all_groups, "%s (%d) (%d),",
-	                        gc->name,
-	                        gc->num, client_count);
+		snprintf(key, sizeof key, "groups.%s.%s", gc->name, "number");
+		json_object_dotset_number(json_obj, key, gc->num);
+
+		snprintf(key, sizeof key, "groups.%s.%s", gc->name, "clients");
+		json_object_dotset_value(json_obj, key, json_value_init_array());
+		clients = json_object_dotget_array(json_obj, key);
+
+		TAILQ_FOREACH(ci, &gc->clientq, group_entry) {
+			json_array_append_string(clients, ci->name);
+			snprintf(key, sizeof key, "groups.%s.%s", gc->name,
+					"is_urgent");
+			json_object_dotset_boolean(json_obj, key,
+					ci->flags & CLIENT_URGENCY);
+		}
+
+		snprintf(key, sizeof key, "groups.%s.%s", gc->name,
+					"is_active");
+		json_object_dotset_boolean(json_obj, key,
+					gc->flags & GROUP_ACTIVE);
+
+		snprintf(key, sizeof key, "groups.%s.%s", gc->name,
+					"is_current");
+		json_object_dotset_boolean(json_obj, key,
+					gc == sc->group_current);
+
+		snprintf(key, sizeof key, "groups.%s.%s", gc->name,
+					"number_of_clients");
+		json_object_dotset_number(json_obj, key,
+					json_array_get_count(clients));
 	}
-	/* Remove any trailing commas. */
-	if (urgencies != NULL)
-	        urgencies[strlen(urgencies) - 1] = '\0';
-	if (all_groups != NULL)
-	        all_groups[strlen(all_groups) - 1] = '\0';
-	if (active_groups != NULL)
-	        active_groups[strlen(active_groups) - 1] = '\0';
 
-	fprintf(sc->status_fp, "urgency:%s|", urgencies != NULL ? urgencies : "");
-	fprintf(sc->status_fp, "desktops:%s|", all_groups != NULL ? all_groups : "");
-	fprintf(sc->status_fp, "active_desktops:%s|", active_groups != NULL ?
-	                active_groups : "");
-	fprintf(sc->status_fp, "current_desktop:%s", sc->group_current->name);
-	fprintf(sc->status_fp, "\n");
+	json_out_str = json_serialize_to_string(json_root);
+	fprintf(sc->status_fp, "%s\n", json_out_str);
 	fflush(sc->status_fp);
-
-	free(urgencies);
-	free(all_groups);
-	free(active_groups);
-}
-
-static void
-u_append_str(char **append, const char *fmt, ...)
-{
-	char	*temp, *result;
-	va_list	 ap;
-
-	va_start(ap, fmt);
-	vasprintf(&temp, fmt, ap);
-	va_end(ap);
-
-	/* Big enough on the first iteration to hold the value of temp. */
-	if (*append == NULL)
-		*append = xcalloc(1, strlen(temp) + 1);
-
-	/* We only append the string if it's not already there. */
-	if (strstr(*append, temp) == NULL)
-		xasprintf(&result, "%s%s", *append, temp);
-	free(temp);
-	free(*append);
-
-	*append = xstrdup(result);
 }
